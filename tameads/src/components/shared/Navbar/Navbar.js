b4c2ad3d8c2484a403c31ac2c55fcded2ab276b0
isakrexha1 <rexhaisak@gmail.com>
2024-02-27 14:10:03 +0100

    menu responsive

--- tameads/src/components/shared/Navbar/Navbar.js
+++ tameads/src/components/shared/Navbar/Navbar.js
@@ -1,48 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-const nav = () => {
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
       <Link to="/" className="nav-logo flex">
         <div className="logo"></div>
       </Link>
 
-      <div className="nav-links flex">
-        
+      <div className="hamburger" onClick={handleHamburgerClick}>
+        <div className={`line ${menuOpen ? "line-rotated" : ""}`}></div>
+        <div className={`line line2 ${menuOpen ? "line2-rotated" : ""}`}></div>
+        <div className={`line ${menuOpen ? "line-rotated" : ""}`}></div>
+      </div>
+
+      <div className={`nav-links flex ${menuOpen ? "nav-links-visible" : "nav-links-hidden"}`}>
         <Link to="" >Advertisers
-        <div className="arr-down"></div></Link>
+          <div className="arr-down"></div></Link>
 
         <Link to="" >Media Owners
-        <div className="arr-down"></div></Link>
+          <div className="arr-down"></div></Link>
 
         <Link to="" >About Us
-        <div className="arr-down"></div></Link>
+          <div className="arr-down"></div></Link>
 
         <Link to=""> Resources
-        <div className="arr-down"></div></Link>
+          <div className="arr-down"></div></Link>
 
         <Link to="">Ad Formats</Link>
       </div>
 
-      <div className="nav-auth flex">
-       <Link to="" >  <div className="headphone"></div>
-       Contact</Link>
-        
+      <div className={`nav-auth flex ${menuOpen ? "nav-links-visible" : "nav-links-hidden"}`}>
+        <Link to="" className="contact flex" > 
+          <div className="headphone"></div>
+          Contact</Link>
 
-        <div className="sun"></div>
+        <Link><div className="sun"></div></Link>
 
-        <Link to="">Sign Up</Link>
+        <Link to="">Log in</Link>
+        <Link to="" className="sign-up flex">Sign Up</Link>
       </div>
 
-      <div className="hamburger">
-        <div className="line"></div>
-        <div className="line line2"></div>
-        <div className="line"></div>
-      </div>
     </div>
   );
 };
 
-export default nav;
+export default Navbar;
