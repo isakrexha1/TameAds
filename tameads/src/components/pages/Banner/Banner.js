a7802c400a9bc1449829919f802528f7cd62d324
isakrexha1 <rexhaisak@gmail.com>
2024-03-12 17:11:25 +0100

    banner created

--- tameads/src/components/pages/Banner/Banner.js
+++ tameads/src/components/pages/Banner/Banner.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+    <div className="banner">
+      <div className="bn-info">
+        <h1>
+          Better Ads For <span>Better Results.</span>
+        </h1>
+        <p>
+          Drive marketing results faster with TameAds global advertising
+          platform for media, buyers, affiliates ad networks, and publishers.
+        </p>
 
-export default Banner
+        <div className="banner-btn">
+          <Link className="btn-start flex">Get Started</Link>
+          <div className="play-part">
+          <div className="play-btn flex">
+          </div>
+          <Link className="play-link flex">Play Video</Link>
+          </div>
+        </div>
+
+        <div className="dashboard"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
